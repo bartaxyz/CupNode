fbe44481d2b7265981e77d22d5159df814315a56
Ondřej Bárta <email@ondrejbarta.xyz>
2023-04-07 14:36:19 +0200

    Initial Commit

---
+++ CupNode/CupNodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupNodeApp.swift
+//  CupNode
+//
+//  Created by Ondrej Barta on 07.04.23.
+//
+
+import SwiftUI
+
+@main
+struct CupNodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
